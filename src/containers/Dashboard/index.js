acf27eb41ed1f1a6e584688de3cb216f604e1c16
Joe Cox <joecox@live.co.uk>
2021-12-12 23:31:27 +0000

    removed hidden logout button from dashboard

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -5,16 +5,13 @@ import Section from "components/Layout/Section"
 import PageTitle from "components/Layout/PageTitle"
 
 const Dashboard = () => {
-  const { displayName, logout } = useAuth()
+  const { displayName } = useAuth()
 
   return (
     <Section>
       <SiteTitle title="Dashboard | Block." />
       <div className="container mx-auto max-w-2xl">
         <PageTitle>Welcome back {displayName}</PageTitle>
-        <button className="mb-10 hidden" onClick={logout}>
-          Logout
-        </button>
         <Posts />
       </div>
     </Section>
